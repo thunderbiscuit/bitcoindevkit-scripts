8f8eceeb037ae50e64ead3afc7b9d2c7d732fa3b
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-01-06 11:06:50 -0500

    Add Rust balance script

---
+++ Rs-1.balance.rs
@@ -0,0 +1,32 @@
+#!/usr/bin/env rust-script
+
+//! bdk 0.26.0
+//! 
+//! Query an Electrum server for the total balance of a wallet.
+//! 
+//! ```cargo
+//! [dependencies]
+//! bdk = "0.26.0"
+//! ```
+
+use bdk::{Wallet, SyncOptions};
+use bdk::database::MemoryDatabase;
+use bdk::blockchain::ElectrumBlockchain;
+use bdk::electrum_client::Client;
+
+fn main() -> Result<(), bdk::Error> {
+    let client = Client::new("ssl://electrum.blockstream.info:60002")?;
+    let blockchain = ElectrumBlockchain::from(client);
+    let wallet = Wallet::new(
+        "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)",
+        Some("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)"),
+        bdk::bitcoin::Network::Testnet,
+        MemoryDatabase::default(),
+    )?;
+
+    wallet.sync(&blockchain, SyncOptions::default())?;
+
+    println!("Descriptor balance: {} SAT", wallet.get_balance()?);
+
+    Ok(())
+}
