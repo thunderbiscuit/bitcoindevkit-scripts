8b2b1b91aa2706d3113d372f703d9c7b4929c5a0
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-06-21 15:34:22 -0400

    Update two Kotlin scripts to alpha 11

--- kotlin/1.balance.main.kts
+++ kotlin/1.balance.main.kts
@@ -1,37 +1,25 @@
 /**
- * bdk-jvm 0.29.2
+ * bdk-jvm 1.0.0-alpha.11
  *
  * Query an Electrum server for the total balance of a wallet using a descriptor.
  */
 
-@file:DependsOn("org.bitcoindevkit:bdk-jvm:0.29.2")
+@file:DependsOn("org.bitcoindevkit:bdk-jvm:1.0.0-alpha.11")
 
-import org.bitcoindevkit.*
+import org.bitcoindevkit.Descriptor
+import org.bitcoindevkit.Wallet
+import org.bitcoindevkit.ElectrumClient
+import org.bitcoindevkit.FullScanRequest
+import org.bitcoindevkit.Network
 
-val descriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)", Network.TESTNET)
-val electrumUrl = "ssl://electrum.blockstream.info:60002"
-val blockchainConfig = BlockchainConfig.Electrum(ElectrumConfig(electrumUrl, null, 5u, null, 100u, true))
-val blockchain = Blockchain(blockchainConfig)
-val databaseConfig = DatabaseConfig.Memory
+val descriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)", Network.SIGNET)
+val changeDescriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/1/*)", Network.SIGNET)
 
-val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
+val wallet: Wallet = Wallet.newNoPersist(descriptor, changeDescriptor, Network.SIGNET)
+val electrumClient: ElectrumClient = ElectrumClient("ssl://mempool.space:60602")
+val fullScanRequest: FullScanRequest = wallet.startFullScan()
+val update = electrumClient.fullScan(fullScanRequest, 10uL, 10uL, false)
+wallet.applyUpdate(update)
 
-wallet.sync(
-    blockchain = blockchain, 
-    progress = null
-)
-
-val balance = wallet.getBalance().total
-println("The wallet balance is $balance")
-
-// For blockchains that provide a progress update (Bitcoin daemon RPC), you can provide one a callback:
-// object LogProgress : Progress {
-//     override fun update(progress: Float, message: String?) {
-//         println("Syncing... $progress")
-//     }
-// }
-
-// wallet.sync(
-//     blockchain = blockchain,
-//     progress = LogProgress
-// )
+val balanceSatoshi = wallet.getBalance().total.toSat()
+println("Balance: $balanceSatoshi satoshi")
--- kotlin/3.getaddress.main.kts
+++ kotlin/3.getaddress.main.kts
@@ -1,28 +1,27 @@
 /**
- * bdk-jvm 0.29.2
+ * bdk-jvm 1.0.0-alpha.11
  *
  * Get an unused address from a wallet.
  */
 
-@file:DependsOn("org.bitcoindevkit:bdk-jvm:0.29.2")
+@file:DependsOn("org.bitcoindevkit:bdk-jvm:1.0.0-alpha.11")
 
-import org.bitcoindevkit.*
+import org.bitcoindevkit.Descriptor
+import org.bitcoindevkit.Wallet
+import org.bitcoindevkit.ElectrumClient
+import org.bitcoindevkit.FullScanRequest
+import org.bitcoindevkit.Network
+import org.bitcoindevkit.KeychainKind
+import org.bitcoindevkit.AddressInfo
 
-val descriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)", Network.TESTNET)
-val electrumUrl = "ssl://electrum.blockstream.info:60002"
-val blockchainConfig = BlockchainConfig.Electrum(ElectrumConfig(electrumUrl, null, 5u, null, 100u, true))
-val blockchain = Blockchain(blockchainConfig)
-val databaseConfig = DatabaseConfig.Memory
+val descriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)", Network.SIGNET)
+val changeDescriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/1/*)", Network.SIGNET)
 
-object LogProgress : Progress {
-    override fun update(progress: Float, message: String?) {
-        println("Syncing...")
-    }
-}
+val wallet: Wallet = Wallet.newNoPersist(descriptor, changeDescriptor, Network.SIGNET)
+val electrumClient: ElectrumClient = ElectrumClient("ssl://mempool.space:60602")
+val fullScanRequest: FullScanRequest = wallet.startFullScan()
+val update = electrumClient.fullScan(fullScanRequest, 10uL, 10uL, false)
+wallet.applyUpdate(update)
 
-val wallet = Wallet(descriptor, null, Network.TESTNET, databaseConfig)
-
-wallet.sync(blockchain, LogProgress)
-
-val lastUnusedAddress: AddressInfo = wallet.getAddress(AddressIndex.LastUnused)
+val lastUnusedAddress: AddressInfo = wallet.revealNextAddress(KeychainKind.EXTERNAL)
 println("The last unused address for this wallet is ${lastUnusedAddress.address.asString()} at index ${lastUnusedAddress.index}")
